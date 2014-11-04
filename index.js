fa2505dbd4386332128f3a1d89a861a56a0ac995
Tasterz <nutss87@gmail.com>
2014-11-05 00:04:50 +0700

    edit index.js

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 var express=require('express')
 var app= express()
 
+
 app.use(express.static(__dirname+'/public'));
+
+app.get('/api/book',function(req,res){
+	var books = [
+		{title: 'DotA Trick 1', price: 200},
+		{title: 'DotA Update', price: 300},
+		{title: 'DotA Pro', price: 400}
+	];
+	res.send(books);
+})
 
 var server=app.listen(3000,function(){
 	console.log("server is running")
